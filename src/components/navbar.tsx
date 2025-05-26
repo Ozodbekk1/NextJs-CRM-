df3e88c56c707a43f4031ed46d5304ad93cb3ea7
Sodiqjon Rasulov <sodiqjon.rasulov@coreteam.uz>
2025-05-26 22:17:00 +0500

    layout changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,36 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { HelpCircle, Settings } from "lucide-react";
 
 export const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <div className="flex items-center h-[7vh] fixed left-0 top-0 right-0  z-30 bg-[#f6f9fc] dark:bg-[#1a1a1a]">
+      <div className="flex items-center justify-between my-4 mx-6 w-full">
+        <Link href="/">
+          <div className="flex items-center">
+            <Image src="/img/logo.png" alt="logo" width={40} height={40} />
+            <span className="text-2xl font-bold pl-2 opacity-75">Drive</span>
+          </div>
+        </Link>
+
+        <div className="flex items-center space-x-2">
+          <div
+            className="p-2 hover:bg-secondary rounded-full transition"
+            role="button"
+          >
+            <HelpCircle className="h-5 w-5" />
+          </div>
+          <div
+            className="p-2 hover:bg-secondary rounded-full transition"
+            role="button"
+          >
+            <Settings className="h-5 w-5" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
