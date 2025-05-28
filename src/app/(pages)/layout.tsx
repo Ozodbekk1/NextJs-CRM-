58cb9d98d99b40100b50be50aaf1c3bee1478317
Ozodbekk1 <olimjonovozodbek16@gmail.com>
2025-05-28 21:50:41 +0500

    All Departments

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,4 +1,7 @@
+/** @format */
+
 import { Navbar, Sidebar } from "../components";
+import { HydrationProvider } from "react-hydration-provider";
 
 export default function RootLayout({
   children,
@@ -6,15 +9,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="min-h-screen relative overflow-x-hidden">
-        <Sidebar />
-        <div className="lg:ml-64 transition-all duration-300">
-          <Navbar />
-          <main className="mt-[7vh] p-6 bg-white dark:bg-[#16151C]">
-            {children}
-          </main>
-        </div>
+    <html lang='en'>
+      <body className='min-h-screen relative overflow-x-hidden'>
+        <HydrationProvider>
+          <Sidebar />
+          <div className='lg:ml-64 transition-all duration-300'>
+            <Navbar />
+            <main className='mt-[7vh] p-6 bg-white dark:bg-[#16151C]'>
+              {children}
+            </main>
+          </div>
+        </HydrationProvider>
       </body>
     </html>
   );
