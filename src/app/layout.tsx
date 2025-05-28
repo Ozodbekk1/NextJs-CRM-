6b0c78398fc59d975d181b363d02c48439c88864
Rasulov <jamshidbekrasulovuz@gmail.com>
2025-05-28 20:51:28 +0500

    layout+empoyees page responsive fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,15 @@ import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import NextTopLoader from "nextjs-toploader";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "HRMS",
